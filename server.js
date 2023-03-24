c0fb24c10a05eb3dfa3f7dc47ecbf1dcaf03fb64
Vikas Deshmukh <114295008+vikasdeshmukh63@users.noreply.github.com>
2023-03-24 22:05:26 +0530

    cors package added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
+app.use(cors());
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 port = process.env.PORT || 5000;
